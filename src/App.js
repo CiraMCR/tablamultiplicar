23d4334b9424ceeba61958ff3cdfff553f3695d0
Aldair G. Ortega <aldairgutierrez509@gmail.com>
2021-09-13 22:50:29 -0500

    ya a ver si sale

--- src/App.js
+++ src/App.js
@@ -4,24 +4,26 @@ import './App.css';
 function App() {
     return ( <
         div className = "App" >
-
         <
+        a > holaaaaaaaaa < /a> <
+        header > hola < /header> <
         header className = "App-header" >
+
         <
         img src = { logo }
         className = "App-logo"
         alt = "logo" / >
         <
         p >
-        Edit < code > src / App.js < /code> and save to reload. <
-        /p> <
+        Edit < code > src / App.js < /code> and save to reload. < /
+        p > <
         a className = "App-link"
         href = "https://reactjs.org"
         target = "_blank"
         rel = "noopener noreferrer" >
-        Learn React <
-        /a> <
-        /header> <
+        Learn React hola a ver si notas los cambios <
+        /a> < /
+        header > <
         /div>
     );
 }
